44cfbea2c2e18cb40333643903466bc51395ba3f
timinoc <timinoc@gmail.com>
2016-11-21 19:10:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ function postMessage() {
 
   body = {
     "bot_id" : botID,
-    "text" : botResponse
+    "text" : "Hello World"
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
